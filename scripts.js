c825ee3441a449a143dc86c7d9747b9d56b44879
Jan <kankovskyj@email.cz>
2019-11-13 21:20:38 +0100

    a commit
    d

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-alert("Hello world!");
+// alert('hello there')
+bck-top = document.getElementById ("back-top-btn");
+
+
+var backTop = document.getElementById('back-top-btn').click();
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
